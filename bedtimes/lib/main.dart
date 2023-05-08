7fa7873b7348aa0801264f628cedb52f11133bee
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-05-07 20:54:18 -0400

    shift everything to the top

--- bedtimes/lib/main.dart
+++ bedtimes/lib/main.dart
@@ -84,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: contents,
         ),
       ),
